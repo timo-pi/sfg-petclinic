d1b43c9b8454f96d5337d1a7fcdc4029c31fd09b
timo-pi <piechotta@mediamarktsaturn.com>
2019-09-03 11:31:05 +0200

    initial commit

--- src/main/java/com/springframeworkguru/petclinic/PetclinicApplication.java
+++ src/main/java/com/springframeworkguru/petclinic/PetclinicApplication.java
@@ -8,5 +8,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
     public static void main(String[] args) {
         SpringApplication.run(PetclinicApplication.class, args);
     }
-
+//...
 }
